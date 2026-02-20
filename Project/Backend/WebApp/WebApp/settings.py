8545823ffb663ecae36b932d0f59e916a8084625
Ganesh-Vankudoth <gvankadoth@gmail.com>
2026-02-20 12:41:19 +0530

    created a resume model also created .env and added files to .gitignore

--- Project/Backend/WebApp/WebApp/settings.py
+++ Project/Backend/WebApp/WebApp/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'firstapp',
+    'resume',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +81,12 @@ WSGI_APPLICATION = 'WebApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
@@ -119,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ Project/Backend/WebApp/resume/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Resume(models.Model):
+    ROLE_CHOICES=[
+        ('python_dev','Python Developer'),
+        ('data_analyst',"Data Analyst"),
+        ('frontend_dev','Frontend Developer'),
+        ('backend_dev','backend Developer')
+    ]
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    file=models.FileField(upload_to='resumes/')
+    job_role=models.TextField(max_length=50,choices=ROLE_CHOICES)
+    score=models.IntegerField(default=0)
+    matched_keywords=models.TextField(blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.job_role}"
+
